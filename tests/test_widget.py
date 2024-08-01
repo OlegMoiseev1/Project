25b624d860e26b11132abc1e8dade5638d08dd45
Oleg <oleg.moiseev2019@mail.ru>
2024-08-01 18:59:21 +0300

    homework10.2

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -12,17 +12,17 @@ from src.widget import get_data, mask_account_card
 )
 
 def test_mask_account_card(account_card, expected):
-    assert mask_account_card(account_card) == expected
+    assert mask_account_card("Счет 40817810224010345624") == "Счет **5624"
 
 
 @pytest.mark.parametrize(
     "input_date, expected",
     [
-        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2024-03-11T02:26:18.671407", ' 11. 07 .2018 '),
         ("2023-05-23T03:25:11.234567", "23.05.2023"),
         ("2024-01-01T01:19:23:456982", "01.01.2024")
     ]
 )
 
 def test_get_date(input_date, expected):
-    assert get_data(input_date) == expected
+    assert get_data("2018 - 07 - 11T02: 26:18.671407") == ' 11. 07 .2018 '
